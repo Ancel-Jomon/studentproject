e65e66e3d703f63e3d3e8d3b0021513f4f298f69
Ancel Puthoor <ancelputhoor88@gmail.com>
2024-12-07 21:23:42 +0530

    added checkbox field

--- src/forms/checkboxfield.tsx
+++ src/forms/checkboxfield.tsx
@@ -3,9 +3,16 @@ import { ErrorMessage, Field } from "formik";
 export default function CheckboxField(props: checkboxFieldProps) {
   return (
     <div className="mb-3 form-check">
-      <Field className="form-check-input" id={props.field} name={props.field} type="checkbox"/>
-        <label htmlFor={props.field}>{props.displayname}</label>
-        <ErrorMessage name={props.field}>{(msg)=><div className="text-danger">{msg}</div>}</ErrorMessage>
+      <Field
+        className="form-check-input"
+        id={props.field}
+        name={props.field}
+        type="checkbox"
+      />
+      <label htmlFor={props.field}>{props.displayname}</label>
+      <ErrorMessage name={props.field}>
+        {(msg) => <div className="text-danger">{msg}</div>}
+      </ErrorMessage>
     </div>
   );
 }
